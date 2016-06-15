7f783c4f55c8a31488da22c19d5f2c5d22266a2e
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-06-15 16:47:53 -0300

    Panel class refactor

--- src/Panel.java
+++ src/Panel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
@@ -8,8 +10,11 @@ import javax.swing.JPanel;
 
 public class Panel extends JPanel {
 
+	private static final long serialVersionUID = 1L;
 	private Image block;
 	private Board board;
+
+	private static final int BLOCK_SIZE = 30;
 
 	public Panel(Board board) {
 		super();
@@ -20,6 +25,8 @@ public class Panel extends JPanel {
 			System.out.println(e.getMessage());
 		}
 		this.board = board;
+
+		this.setPreferredSize(new Dimension(BLOCK_SIZE * 10, BLOCK_SIZE * 20));
 	}
 
 	public void draw() {
@@ -33,7 +40,8 @@ public class Panel extends JPanel {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
 				if (matrix[i][j]) {
-					g.drawImage(block, 30 * j, 30 * i, 30, 30, this);
+					g.drawImage(block, BLOCK_SIZE * j, BLOCK_SIZE * i,
+							BLOCK_SIZE, BLOCK_SIZE, this);
 				}
 			}
 		}
